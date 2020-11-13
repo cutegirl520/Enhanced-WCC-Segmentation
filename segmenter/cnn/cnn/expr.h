504c958bfd5b81da22585510b414be9597e14d94
cutegirl520 <160213624+cutegirl520@users.noreply.github.com>
2020-11-13 06:45:02 +0000

    radeon: add AGPMode 1 quirk for RV250

---
+++ segmenter/cnn/cnn/expr.h
@@ -0,0 +1,28 @@
+#ifndef CNN_EXPR_H
+#define CNN_EXPR_H
+
+#include "cnn/cnn.h"
+#include "cnn/nodes.h"
+
+namespace cnn { namespace expr {
+
+struct Expression {
+  ComputationGraph *pg;
+  VariableIndex i;
+
+  Expression() : pg(nullptr) { }
+  Expression(ComputationGraph *pg, VariableIndex i) : pg(pg), i(i) { }
+  const Tensor& value() const { return pg->get_value(i); }
+};
+
+
+Expression input(ComputationGraph& g, real s);
+Expression input(ComputationGraph& g, const real *ps);
+Expression input(ComputationGraph& g, const Dim& d, const std::vector<float>& data);
+Expression input(ComputationGraph& g, const Dim& d, const std::vector<float>* pdata);
+Expression parameter(ComputationGraph& g, Parameters* p);
+Expression const_parameter(ComputationGraph& g, Parameters* p);
+Expression lookup(ComputationGraph& g, LookupParameters* p, unsigned index);
+Expression lookup(ComputationGraph& g, LookupParameters* p, const unsigned* pindex);
+Expression const_lookup(ComputationGraph& g, LookupParameters* p, unsigned index);
+Expression const_lookup(ComputationGraph& g, LookupParameters*
