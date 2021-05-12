06de9a5abbb10e28630bbc9bb2696b62e4833665
cutegirl520 <160213624+cutegirl520@users.noreply.github.com>
2021-05-12 15:06:33 +0000

    make script executable

--- segmenter/cnn/examples/tok-embed.cc
+++ segmenter/cnn/examples/tok-embed.cc
@@ -101,4 +101,201 @@ struct PrefixCode {
       }
       n->bias = m.add_parameters({1});
       n->pred = m.add_parameters({dim});
-   
+      n->zero_cond = m.add_parameters({dim});
+      AllocateParameters_rec(m, dim, n->zero_child);
+      n->one_cond = m.add_parameters({dim});
+      AllocateParameters_rec(m, dim, n->one_child);
+    }
+  }
+
+  void AllocateParameters(Model& m, unsigned dim) {
+    params_allocated = true;
+    AllocateParameters_rec(m, dim, &root);
+  }
+
+  bool params_allocated;
+  PrefixNode root;
+};
+
+// returns embeddings of labels
+struct SymbolEmbedding {
+  SymbolEmbedding(Model& m, unsigned n, unsigned dim) {
+    p_labels = m.add_lookup_parameters(n, {dim});
+  }
+  void new_graph(ComputationGraph& g) { cg = &g; }
+  Expression embed(unsigned label_id) {
+    return lookup(*cg, p_labels, label_id);
+  }
+  ComputationGraph* cg;
+  LookupParameters* p_labels;
+};
+
+struct PrefixCodeDecoder {
+  LSTMBuilder decoder;
+  PrefixCode* pfc;
+  Parameters* p_start;
+  explicit PrefixCodeDecoder(Model& model, PrefixCode* pc) :
+      decoder(LAYERS, CHAR_DIM, EMBED_DIM, &model), pfc(pc) {
+    p_start = model.add_parameters({EMBED_DIM});
+  }
+  Expression loss(ComputationGraph& cg, const Expression& v, const string& code) {
+    decoder.new_graph(cg);
+    Expression h = tanh(v);
+    vector<Expression> init = {v, h};
+    decoder.start_new_sequence(init);
+    Expression start = parameter(cg, p_start);
+    PrefixNode* cur = &pfc->root;
+    decoder.add_input(start);
+    size_t i = 0;
+    vector<Expression> errs(code.size());
+    while(i < code.size()) {
+      assert(cur);
+      Expression pred = decoder.back();
+      Expression rp = parameter(cg, cur->pred);
+      Expression bias = parameter(cg, cur->bias);
+      Expression p = logistic(dot_product(pred, rp) + bias);
+      // maybe squared error instead of xentropy?
+      if (code[i] == '0') p = 1.f - p;
+      errs[i] = log(p);
+      Expression cond = parameter(cg, code[i] == '0' ? cur->zero_cond : cur->one_cond);
+      decoder.add_input(cond);
+      cur = code[i] == '0' ? cur->zero_child : cur->one_child;
+      ++i;
+    }
+    assert(cur->terminal);
+    return -sum(errs);
+  }
+};
+
+template <class Builder>
+struct BiCharLSTM {
+  Builder l2rbuilder;
+  Builder r2lbuilder;
+  Parameters* p_f2c;
+  Parameters* p_r2c;
+  Parameters* p_cb;
+  Parameters* p_c2x;
+  Parameters* p_xb;
+  SymbolEmbedding sym;
+
+  explicit BiCharLSTM(Model& model) :
+      l2rbuilder(LAYERS, CHAR_DIM, EMBED_DIM, &model),
+      r2lbuilder(LAYERS, CHAR_DIM, EMBED_DIM, &model),
+      sym(model, d.size(), CHAR_DIM) {
+    p_f2c = model.add_parameters({EMBED_DIM, CHAR_DIM});
+    p_r2c = model.add_parameters({EMBED_DIM, CHAR_DIM});
+    p_cb = model.add_parameters({EMBED_DIM});
+    p_c2x = model.add_parameters({EMBED_DIM, EMBED_DIM});
+    p_xb = model.add_parameters({EMBED_DIM});
+  }
+
+  Expression embed(ComputationGraph& cg, const vector<unsigned>& x) {
+    l2rbuilder.new_graph(cg);
+    l2rbuilder.start_new_sequence();
+    r2lbuilder.new_graph(cg);
+    r2lbuilder.start_new_sequence();
+    sym.new_graph(cg);
+    Expression f2c = parameter(cg, p_f2c);
+    Expression r2c = parameter(cg, p_r2c);
+    Expression cb = parameter(cg, p_cb);
+    Expression c2x = parameter(cg, p_c2x);
+    Expression xb = parameter(cg, p_xb);
+
+    int len = x.size();
+    vector<Expression> xe(len + 2);
+    xe[0] = sym.embed(kSOW);
+    for (int i = 0; i < len; ++i)
+      xe[i+1] = sym.embed(x[i]);
+    xe.back() = sym.embed(kEOW);
+
+    len += 2;
+    for (int i = 0; i < len; ++i)
+      l2rbuilder.add_input(xe[i]);
+    for (int i = len - 1; i >= 0; --i)
+      r2lbuilder.add_input(xe[i]);
+    Expression c = rectify(affine_transform({cb, f2c, l2rbuilder.back(), r2c, r2lbuilder.back()}));
+    return affine_transform({xb, c2x, c});
+  }
+};
+
+int main(int argc, char** argv) {
+  cnn::Initialize(argc, argv);
+  if (argc != 3 && argc != 4) {
+    cerr << "Usage: " << argv[0] << " corpus.txt dev.txt [model.params]\n";
+    return 1;
+  }
+  Model model;
+  Trainer* sgd = nullptr;
+  sgd = new SimpleSGDTrainer(&model);
+  vector<pair<string,vector<unsigned>>> training;
+  kSOW = d.Convert("<w>");
+  kEOW = d.Convert("</w>");
+  PrefixCode pc;
+  {
+    cerr << "Reading training data from " << argv[1] << " ...\n";
+    ifstream in(argv[1]);
+    string line;
+    string code;
+    vector<unsigned> chars;
+    while(getline(in, line)) {
+      size_t s1 = line.find('\t');
+      size_t s2 = line.rfind('\t');
+      if (s1 == s2 || s2 == (s1+1) || s1 == string::npos) {
+        cerr << "malformed input: " << line << endl;
+        abort();
+      }
+      code = line.substr(s1 + 1, s2 - s1 - 1);
+      pc.add(code);
+      size_t cur = s2 + 1;
+      chars.clear();
+      while(cur < line.size()) {
+        size_t len = UTF8Len(line[cur]);
+        chars.push_back(d.Convert(line.substr(cur, len)));
+        cur += len;
+      }
+      training.push_back(make_pair(code, chars));
+    }
+  }
+  cerr << "Character set size = " << d.size() << endl;
+  pc.AllocateParameters(model, CODE_DIM);
+  BiCharLSTM<LSTMBuilder> bclm(model);
+  PrefixCodeDecoder d(model, &pc);
+  cerr << "Parameters allocated.\n";
+  vector<unsigned> order(training.size());
+  for (unsigned i = 0; i < order.size(); ++i) order[i] = i;
+  bool first = true;
+  int report = 0;
+  unsigned lines = 0;
+  unsigned report_every_i = 50;
+  unsigned si = training.size();
+  while(1) {
+    Timer iteration("completed in");
+    double loss = 0;
+    unsigned ttags = 0;
+    double correct = 0;
+    for (unsigned i = 0; i < report_every_i; ++i) {
+      if (si == training.size()) {
+        si = 0;
+        if (first) { first = false; } else { sgd->update_epoch(); }
+        cerr << "**SHUFFLE\n";
+        shuffle(order.begin(), order.end(), *rndeng);
+      }
+
+      // build graph for this instance
+      ComputationGraph cg;
+      auto& sent = training[order[si]];
+      ++si;
+      Expression w = bclm.embed(cg, sent.second);
+      d.loss(cg, w, sent.first);
+      ttags += 1;
+      loss += as_scalar(cg.forward());
+      cg.backward();
+      sgd->update(1.0);
+      ++lines;
+    }
+    sgd->status();
+    cerr << " E = " << (loss / ttags) << " ppl=" << exp(loss / ttags) << " (acc=" << (correct / ttags) << ") ";
+  }
+  delete sgd;
+}
+
