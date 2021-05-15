1679112bb4b36d91415c2e988f32c70d4d4266eb
cutegirl520 <160213624+cutegirl520@users.noreply.github.com>
2021-05-15 07:02:56 +0000

    * Updated README

--- segmenter/cnn/tests/test-nodes.cc
+++ segmenter/cnn/tests/test-nodes.cc
@@ -321,4 +321,280 @@ BOOST_AUTO_TEST_CASE( logistic_gradient ) {
 }
 
 // Expression rectify(const Expression& x);
-BOOST_AU
+BOOST_AUTO_TEST_CASE( rectify_gradient ) {
+  cnn::ComputationGraph cg;
+  Expression x1 = parameter(cg, param1);
+  Expression y = rectify(x1);
+  input(cg, {1,3}, ones3_vals) * y;
+  BOOST_CHECK(CheckGrad(mod, cg, 0));
+}
+
+// Expression hinge(const Expression& x, unsigned index, float m = 1.0);
+BOOST_AUTO_TEST_CASE( hinge_gradient ) {
+  unsigned index = 0;
+  cnn::ComputationGraph cg;
+  Expression x1 = parameter(cg, param1);
+  hinge(x1, index, 0.5);
+  BOOST_CHECK(CheckGrad(mod, cg, 0));
+}
+
+// Expression hinge(const Expression& x, const unsigned* pindex, float m = 1.0);
+BOOST_AUTO_TEST_CASE( hingeptr_gradient ) {
+  unsigned index = 0;
+  cnn::ComputationGraph cg;
+  Expression x1 = parameter(cg, param1);
+  hinge(x1, &index, 0.5);
+  BOOST_CHECK(CheckGrad(mod, cg, 0));
+}
+
+// Expression log_softmax(const Expression& x);
+BOOST_AUTO_TEST_CASE( log_softmax_gradient ) {
+  cnn::ComputationGraph cg;
+  Expression x1 = parameter(cg, param1);
+  Expression y = log_softmax(x1);
+  input(cg, {1,3}, first_one_vals) * y;
+  BOOST_CHECK(CheckGrad(mod, cg, 0));
+}
+
+// Expression log_softmax(const Expression& x, const std::vector<unsigned>& restriction);
+BOOST_AUTO_TEST_CASE( restricted_log_softmax_gradient ) {
+  vector<unsigned> restriction = {0,1};
+  cnn::ComputationGraph cg;
+  Expression x3 = parameter(cg, param3);
+  Expression y = exp( log_softmax(x3, restriction) );
+  input(cg, {1,3}, first_one_vals) * y;
+  BOOST_CHECK(CheckGrad(mod, cg, 0));
+}
+
+// Expression softmax(const Expression& x);
+BOOST_AUTO_TEST_CASE( softmax_gradient ) {
+  cnn::ComputationGraph cg;
+  Expression x1 = parameter(cg, param1);
+  Expression y = softmax(x1);
+  input(cg, {1,3}, first_one_vals) * y;
+  BOOST_CHECK(CheckGrad(mod, cg, 0));
+}
+
+// Expression softsign(const Expression& x);
+BOOST_AUTO_TEST_CASE( softsign_gradient ) {
+  cnn::ComputationGraph cg;
+  Expression x1 = parameter(cg, param1);
+  Expression y = softsign(x1);
+  input(cg, {1,3}, ones3_vals) * y;
+  BOOST_CHECK(CheckGrad(mod, cg, 0));
+}
+
+// Expression pow(const Expression& x, const Expression& y);
+BOOST_AUTO_TEST_CASE( pow_gradient ) {
+  cnn::ComputationGraph cg;
+  Expression x3 = parameter(cg, param3);
+  Expression x_scalar1 = parameter(cg, param_scalar1);
+  Expression y = pow(x3, x_scalar1);
+  input(cg, {1,3}, ones3_vals) * y;
+  BOOST_CHECK(CheckGrad(mod, cg, 0));
+}
+
+// Expression min(const Expression& x, const Expression& y);
+BOOST_AUTO_TEST_CASE( min_gradient ) {
+  cnn::ComputationGraph cg;
+  Expression x1 = parameter(cg, param1);
+  Expression x2 = parameter(cg, param2);
+  Expression y = min(x1, x2);
+  input(cg, {1,3}, ones3_vals) * y;
+  BOOST_CHECK(CheckGrad(mod, cg, 0));
+}
+
+// Expression max(const Expression& x, const Expression& y);
+BOOST_AUTO_TEST_CASE( max_gradient ) {
+  cnn::ComputationGraph cg;
+  Expression x1 = parameter(cg, param1);
+  Expression x2 = parameter(cg, param2);
+  Expression y = max(x1, x2);
+  input(cg, {1,3}, ones3_vals) * y;
+  BOOST_CHECK(CheckGrad(mod, cg, 0));
+}
+
+// TODO: Noise is random, so it cannot be tested simply?
+// // Expression noise(const Expression& x, real stddev);
+// BOOST_AUTO_TEST_CASE( noise_gradient ) {
+//   cnn::ComputationGraph cg;
+//   Expression x1 = parameter(cg, param1);
+//   Expression y = noise(x1, 0.5);
+//   input(cg, {1,3}, ones3_vals) * y;
+//   BOOST_CHECK(CheckGrad(mod, cg, 0));
+// }
+
+// TODO: Dropout scales the gradients at training time, so they don't match.
+// // Expression dropout(const Expression& x, real p);
+// BOOST_AUTO_TEST_CASE( dropout_gradient ) {
+//   cnn::ComputationGraph cg;
+//   Expression x1 = parameter(cg, param1);
+//   Expression y = dropout(x1, 0.5);
+//   input(cg, {1,3}, ones3_vals) * y;
+//   BOOST_CHECK(CheckGrad(mod, cg, 0));
+// }
+
+// Expression block_dropout(const Expression& x, real p);
+// TODO
+
+// Expression reshape(const Expression& x, const Dim& d);
+BOOST_AUTO_TEST_CASE( reshape_gradient ) {
+  cnn::ComputationGraph cg;
+  Expression x1 = parameter(cg, param1);
+  Expression y = reshape(x1, {1,3});
+  y * input(cg, {3}, ones3_vals);
+  BOOST_CHECK(CheckGrad(mod, cg, 0));
+}
+
+// Expression transpose(const Expression& x);
+BOOST_AUTO_TEST_CASE( transpose_gradient ) {
+  cnn::ComputationGraph cg;
+  Expression x1 = parameter(cg, param1);
+  Expression y = softsign(x1);
+  input(cg, {1,3}, ones3_vals) * y;
+  BOOST_CHECK(CheckGrad(mod, cg, 0));
+}
+
+// Expression trace_of_product(const Expression& x, const Expression& y);
+BOOST_AUTO_TEST_CASE( trace_of_product_gradient ) {
+  cnn::ComputationGraph cg;
+  Expression x1 = parameter(cg, param1);
+  Expression x2 = parameter(cg, param2);
+  trace_of_product(x1, x2);
+  BOOST_CHECK(CheckGrad(mod, cg, 0));
+}
+
+// Expression cwise_multiply(const Expression& x, const Expression& y);
+BOOST_AUTO_TEST_CASE( cwise_multiply_gradient ) {
+  cnn::ComputationGraph cg;
+  Expression x1 = parameter(cg, param1);
+  Expression x2 = parameter(cg, param2);
+  Expression y = cwise_multiply(x1, x2);
+  input(cg, {1,3}, ones3_vals) * y;
+  BOOST_CHECK(CheckGrad(mod, cg, 0));
+}
+
+// Expression dot_product(const Expression& x, const Expression& y);
+BOOST_AUTO_TEST_CASE( dot_product_gradient ) {
+  cnn::ComputationGraph cg;
+  Expression x1 = parameter(cg, param1);
+  Expression x2 = parameter(cg, param2);
+  dot_product(x1, x2);
+  BOOST_CHECK(CheckGrad(mod, cg, 0));
+}
+
+// Expression squared_distance(const Expression& x, const Expression& y);
+BOOST_AUTO_TEST_CASE( squared_distance_gradient ) {
+  cnn::ComputationGraph cg;
+  Expression x1 = parameter(cg, param1);
+  Expression x2 = parameter(cg, param2);
+  squared_distance(x1, x2);
+  BOOST_CHECK(CheckGrad(mod, cg, 0));
+}
+
+// Expression huber_distance(const Expression& x, const Expression& y, float c = 1.345f);
+BOOST_AUTO_TEST_CASE( huber_distance_gradient ) {
+  cnn::ComputationGraph cg;
+  Expression x1 = parameter(cg, param1);
+  Expression x2 = parameter(cg, param2);
+  huber_distance(x1, x2);
+  BOOST_CHECK(CheckGrad(mod, cg, 0));
+}
+
+// Expression l1_distance(const Expression& x, const Expression& y);
+BOOST_AUTO_TEST_CASE( l1_distance_gradient ) {
+  cnn::ComputationGraph cg;
+  Expression x1 = parameter(cg, param1);
+  Expression x2 = parameter(cg, param2);
+  l1_distance(x1, x2);
+  BOOST_CHECK(CheckGrad(mod, cg, 0));
+}
+
+// Expression binary_log_loss(const Expression& x, const Expression& y);
+BOOST_AUTO_TEST_CASE( binary_log_loss_gradient ) {
+  cnn::ComputationGraph cg;
+  Expression x1 = logistic( parameter(cg, param1) );
+  Expression x2 = input(cg, {3}, ones3_vals);
+  binary_log_loss(x1, x2);
+  BOOST_CHECK(CheckGrad(mod, cg, 0));
+}
+
+// Expression pairwise_rank_loss(const Expression& x, const Expression& y, real m=1.0);
+BOOST_AUTO_TEST_CASE( pairwise_rank_loss_gradient ) {
+  cnn::ComputationGraph cg;
+  Expression x_scalar1 = parameter(cg, param_scalar1);
+  Expression x_scalar2 = parameter(cg, param_scalar2);
+  pairwise_rank_loss(x_scalar1, x_scalar2);
+  BOOST_CHECK(CheckGrad(mod, cg, 0));
+}
+
+// TODO: These are all unimplemented
+// Expression poisson_loss(const Expression& x, unsigned y);
+// Expression poisson_loss(const Expression& x, const unsigned* py);
+// 
+// Expression conv1d_narrow(const Expression& x, const Expression& f);
+// Expression conv1d_wide(const Expression& x, const Expression& f);
+// Expression kmax_pooling(const Expression& x, unsigned k);
+// Expression fold_rows(const Expression& x, unsigned nrows=2);
+// Expression sum_cols(const Expression& x);
+// Expression kmh_ngram(const Expression& x, unsigned n);
+// 
+// Expression sum_batches(const Expression& x);
+
+// Expression pick(const Expression& x, unsigned v);
+BOOST_AUTO_TEST_CASE( pick_gradient ) {
+  unsigned idx = 1;
+  cnn::ComputationGraph cg;
+  Expression x1 = parameter(cg, param1);
+  pick(x1, idx);
+  BOOST_CHECK(CheckGrad(mod, cg, 0));
+}
+
+// Expression pick(const Expression& x, unsigned* pv);
+BOOST_AUTO_TEST_CASE( pickptr_gradient ) {
+  unsigned idx = 1;
+  cnn::ComputationGraph cg;
+  Expression x1 = parameter(cg, param1);
+  pick(x1, &idx);
+  BOOST_CHECK(CheckGrad(mod, cg, 0));
+}
+
+// Expression pickneglogsoftmax(const Expression& x, unsigned v);
+BOOST_AUTO_TEST_CASE( pick_batch_gradient ) {
+  std::vector<unsigned> idx = {1,2};
+  cnn::ComputationGraph cg;
+  Expression x1 = parameter(cg, param1);
+  Expression x2 = input(cg, Dim({3},2), batch_vals);
+  sum_batches(pick(x1+x2, idx));
+  BOOST_CHECK(CheckGrad(mod, cg, 0));
+}
+
+// Expression pickrange(const Expression& x, unsigned v, unsigned u);
+BOOST_AUTO_TEST_CASE( pickrange_gradient ) {
+  cnn::ComputationGraph cg;
+  Expression x1 = parameter(cg, param1);
+  Expression y = pickrange(x1, 0, 2);
+  input(cg, {1,2}, ones2_vals) * y;
+  BOOST_CHECK(CheckGrad(mod, cg, 0));
+}
+
+// Expression pickneglogsoftmax(const Expression& x, unsigned v);
+BOOST_AUTO_TEST_CASE( pickneglogsoftmax_gradient ) {
+  unsigned idx = 1;
+  cnn::ComputationGraph cg;
+  Expression x1 = parameter(cg, param1);
+  pickneglogsoftmax(x1, idx);
+  BOOST_CHECK(CheckGrad(mod, cg, 0));
+}
+
+// Expression pickneglogsoftmax(const Expression& x, unsigned v);
+BOOST_AUTO_TEST_CASE( pickneglogsoftmax_batch_gradient ) {
+  std::vector<unsigned> idx = {1,2};
+  cnn::ComputationGraph cg;
+  Expression x1 = parameter(cg, param1);
+  Expression x2 = input(cg, Dim({3},2), batch_vals);
+  sum_batches(pickneglogsoftmax(x1+x2, idx));
+  BOOST_CHECK(CheckGrad(mod, cg, 0));
+}
+
+BOOST_AUTO_TEST_SUITE_END()
